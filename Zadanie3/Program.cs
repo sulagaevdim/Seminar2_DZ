4ddf84ec989beeee20a5c0552a0bc1c93482edfa
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-09 17:25:05 +0400

    add zadanie3

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Console.Write("Введите двузначное число: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+
+if (n > 10 && n < 100){
+    int first = n / 10;
+    int second = n % 10;
+    if (first > second) {
+        System.Console.WriteLine($"Наибольшая цифра введенного числа - {first}");
+    } else if (first < second) {
+        System.Console.WriteLine($"Наибольшая цифра введенного числа - {second}");
+    } else {
+        System.Console.WriteLine("цифры введенного числа равны");
+    }
+
+} else {
+    System.Console.WriteLine("число не двузначное");
+}
