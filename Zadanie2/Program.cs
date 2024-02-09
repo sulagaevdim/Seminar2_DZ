5e2e3faf24c852e720b41798448a0320a4de38e7
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-09 17:13:19 +0400

    add zadanie2

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт 
+// номер координатной четверти плоскости, в которой находится эта точка.
+
+System.Console.Write("Введите Х: ");
+int x = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите Y: ");
+int y = Convert.ToInt32(System.Console.ReadLine());
+
+if (x == 0 || y == 0){
+    System.Console.WriteLine("Точка расположена на оси кординат");
+}else {
+    if (x > 0)
+    {
+       if (y > 0)
+       {
+           System.Console.WriteLine("Точка в 1 четверти");
+       } else {
+           System.Console.WriteLine("Точка в 4 четверти");
+       }
+    } else {
+       if (y > 0)
+       {
+           System.Console.WriteLine("Точка в 2 четверти");
+       } else {
+           System.Console.WriteLine("Точка в 3 четверти");
+      }
+    }
+}
